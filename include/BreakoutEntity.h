c35c3e4b0d5d4aeb3f215a2f31c7e5026f197fd5
haiwaCase <clement222206@gmail.com>
2021-01-13 20:12:11 +0100

    Add BreakoutEntity class

---
+++ include/BreakoutEntity.h
@@ -0,0 +1,16 @@
+#ifndef __BREAKOUT__ENTITY__H__
+#define __BREAKOUT__ENTITY__H__
+
+#include <SFML/Graphics.hpp>
+
+class BreakoutEntity {
+    protected:
+        sf::Vector2f m_position;
+    
+    public:
+        BreakoutEntity(sf::Vector2f const & position);
+        virtual void update();
+        virtual void render(sf::RenderWindow & renderWindow);
+};
+
+#endif
---
+++ src/BreakoutEntity.cpp
@@ -0,0 +1,10 @@
+#include "BreakoutEntity.h"
+
+BreakoutEntity::BreakoutEntity(sf::Vector2f const & position) : m_position(position) {
+}
+
+void BreakoutEntity::update() {
+}
+
+void BreakoutEntity::render(sf::RenderWindow & renderWindow) {
+}
