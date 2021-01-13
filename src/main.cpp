a54a2397f27247554e06475315385b5fb35aaf54
haiwaCase <clement222206@gmail.com>
2021-01-13 20:01:49 +0100

    Add sfml lib and first window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,33 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main(void) {
-	std::cout << "Hello world!" << std::endl;
-	
+	sf::RenderWindow window{sf::VideoMode(800, 600), "Breakout", sf::Style::Close};
+
+	window.setVerticalSyncEnabled(true);
+
+	while (window.isOpen()) {
+		sf::Event event;
+
+		while (window.pollEvent(event)) {
+			switch (event.type) {
+				case sf::Event::Closed:
+					window.close();
+					break;
+
+				case sf::Event::KeyReleased:
+					if (event.key.code == sf::Keyboard::Escape) {
+						window.close();
+					}
+					break;
+
+				default:
+					break;
+			}
+
+			window.clear();
+			window.display();
+		}
+	}
+
 	return 0;
 }
